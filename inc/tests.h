ac55ee74e7cc0770ce1f585ec2a187fe29ad4a04
Alexandr Khlyzov <alexhlyz@yandex.ru>
2022-04-03 16:23:08 +0500

    Update CMakeLists.txt

--- inc/tests.h
+++ inc/tests.h
@@ -2,64 +2,62 @@
 
 #include <string>
 
-namespace {
-    // "Магическое" число, используемое для отслеживания живости объекта
-    inline const uint32_t DEFAULT_COOKIE = 0xdeadbeef;
+// "Магическое" число, используемое для отслеживания живости объекта
+inline const uint32_t DEFAULT_COOKIE = 0xdeadbeef;
 
-    struct TestObj {
-        TestObj() = default;
+struct TestObj {
+    TestObj() = default;
 
-        TestObj(const TestObj& other) = default;
+    TestObj(const TestObj& other) = default;
 
-        TestObj& operator=(const TestObj& other) = default;
+    TestObj& operator=(const TestObj& other) = default;
 
-        TestObj(TestObj&& other) = default;
+    TestObj(TestObj&& other) = default;
 
-        TestObj& operator=(TestObj&& other) = default;
+    TestObj& operator=(TestObj&& other) = default;
 
-        ~TestObj();
+    ~TestObj();
 
-        [[nodiscard]] bool IsAlive() const noexcept;
+    [[nodiscard]] bool IsAlive() const noexcept;
 
-        uint32_t cookie = DEFAULT_COOKIE;
-    };
+    uint32_t cookie = DEFAULT_COOKIE;
+};
 
-    struct Obj {
-        Obj();
+struct Obj {
+    Obj();
 
-        explicit Obj(int id);
+    explicit Obj(int id);
 
-        Obj(int id, std::string name);
+    Obj(int id, std::string name);
 
-        Obj(const Obj& other);
+    Obj(const Obj& other);
 
-        Obj(Obj&& other) noexcept;
+    Obj(Obj&& other) noexcept;
 
-        Obj& operator=(const Obj& other);
+    Obj& operator=(const Obj& other);
 
-        Obj& operator=(Obj&& other) noexcept;
+    Obj& operator=(Obj&& other) noexcept;
 
-        ~Obj();
+    ~Obj();
 
-        static int GetAliveObjectCount();
+    static int GetAliveObjectCount();
 
-        static void ResetCounters();
+    static void ResetCounters();
 
-        bool throw_on_copy = false;
-        int id = 0;
-        std::string name;
+    bool throw_on_copy = false;
+    int id = 0;
+    std::string name;
 
-        static inline int default_construction_throw_countdown = 0;
-        static inline int num_default_constructed = 0;
-        static inline int num_constructed_with_id = 0;
-        static inline int num_constructed_with_id_and_name = 0;
-        static inline int num_copied = 0;
-        static inline int num_moved = 0;
-        static inline int num_destroyed = 0;
-        static inline int num_assigned = 0;
-        static inline int num_move_assigned = 0;
-    };
-}
+    static inline int default_construction_throw_countdown = 0;
+    static inline int num_default_constructed = 0;
+    static inline int num_constructed_with_id = 0;
+    static inline int num_constructed_with_id_and_name = 0;
+    static inline int num_copied = 0;
+    static inline int num_moved = 0;
+    static inline int num_destroyed = 0;
+    static inline int num_assigned = 0;
+    static inline int num_move_assigned = 0;
+};
 
 void Test1();
 
@@ -73,30 +71,28 @@ void Test5();
 
 void Test6();
 
-namespace {
-    struct C {
-        C() noexcept;
+struct C {
+    C() noexcept;
 
-        C(const C& /*other*/) noexcept;
+    C(const C& /*other*/) noexcept;
 
-        C(C&& /*other*/) noexcept;
+    C(C&& /*other*/) noexcept;
 
-        C& operator=(const C& other) noexcept;
+    C& operator=(const C& other) noexcept;
 
-        C& operator=(C&& /*other*/) noexcept;
+    C& operator=(C&& /*other*/) noexcept;
 
-        ~C();
+    ~C();
 
-        static void Reset();
+    static void Reset();
 
-        inline static size_t def_ctor = 0;
-        inline static size_t copy_ctor = 0;
-        inline static size_t move_ctor = 0;
-        inline static size_t copy_assign = 0;
-        inline static size_t move_assign = 0;
-        inline static size_t dtor = 0;
-    };
-}
+    inline static size_t def_ctor = 0;
+    inline static size_t copy_ctor = 0;
+    inline static size_t move_ctor = 0;
+    inline static size_t copy_assign = 0;
+    inline static size_t move_assign = 0;
+    inline static size_t dtor = 0;
+};
 
 void Dump();
 
